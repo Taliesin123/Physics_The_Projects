a80f535fcf67c7c418e208d99bfb734a9ced646b
Taliesin123 <taliesin.perez@gmail.com>
2026-04-13 15:17:11 +0200

    voila4

--- TO DO.py
+++ TO DO.py
@@ -1,6 +1,6 @@
-13.04.2026:
+Pour fin de semaine 8:
 
-- Finir les plots propres. Tali
+- Finir les premiers plots propres. Tali
 - Faire phase transition avec color map. Tali
-- Rédiger la nouvelle partie théorique du rapport
-- 
+- Rédiger la nouvelle partie théorique du rapport (début, hessienne, méthode spectrale sur Y). Celia
+- Rédiger rapport (matrice de fisher, loss avec les 2 spikes, partie fisher). Tali
