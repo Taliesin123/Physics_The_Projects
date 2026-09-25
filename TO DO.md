886eed9f31688c2d12499f5c97396c7b92781fc9
Taliesin123 <taliesin.perez@gmail.com>
2026-09-25 17:15:40 +0200

    changement sur le fichier todo

---
+++ TO DO.md
@@ -0,0 +1,5 @@
+## To Do: 
+- Faire en sorte que le raport soit hyper clair et détaillé, surtout à partir de la section 3.3.
+- Preuve BBP raport.
+- Preuve rank 2 BBP raport.
+- BONUS:Checker la litérature pour trouver si EWC est bien la meilleure méthode qui existe (il y en a des meilleurs mtn, il faut trouver lesquelles).
--- TO DO.py
+++
@@ -1,6 +0,0 @@
-To Do: 
-- Rédiger dans rapport solutions analytiques vectp et valp de P
-- Dossier : Results, cluster ! A GARDER: plot_loss_vs_t, plot overlap_vs_t et autre si nécessaire; Data, Spike_code, spike_fisher (genre toous quoi) 
-- Comparer methodes pour N taches avec lambda differents (voir spike_lib_Ntasks.py), overlap_comp )
-- comparer sol analytique et numeriques: 2 valeurs proprent, 2 overalps vect propres P;  vs a, b, rho
-- Avec bruit (étude de Y et plus P): (ici lambda est la top val propre de P) BBP transition lambdaout = lambda + 1/lambda (lambda vp de P); pour xhat top eigenvect de Y: compute theory et graphe comparer overlap carré de (xhat et top eigenvect de P) (thoerie: donne potentiellement 1- 1/lambda^2) (comme pour 1 spike)
